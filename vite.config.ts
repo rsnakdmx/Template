135a69f0fd08aee061767558fe7ed4fa7c0f5a37
rsnakdmx <rsnakdmx@hotmail.com>
2024-09-29 01:22:19 -0600

    Se agrega path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
+import * as path from 'path';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/',
+	build: {
+		rollupOptions: {
+			treeshake: 'recommended',
+		},
+	},
 	plugins: [react()],
+	resolve: {
+		alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+	},
 	server: {
 		https: {
 			cert: 'C:/Apache24/conf/server.crt',
