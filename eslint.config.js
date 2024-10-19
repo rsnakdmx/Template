a07eb46d702c7cc23f8deba9417d47b497521376
rsnakdmx <rsnakdmx@hotmail.com>
2024-10-18 20:11:03 -0600

    Se agregaron reglas a eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,44 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import tsparser from '@typescript-eslint/parser';
 import pluginReact from 'eslint-plugin-react';
 import pluginPromise from 'eslint-plugin-promise';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import tsParser from '@typescript-eslint/parser';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
 
 export default [
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommended,
 	pluginReact.configs.flat.recommended,
 	pluginPromise.configs['flat/recommended'],
+	{ ignores: ['coverage/', 'dist/', 'node_modules/', 'public/'] },
 	{ files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-	{ ignores: ['dist/', 'node_modules/', 'public/'] },
 	{
 		languageOptions: {
 			globals: { ...globals.browser, ...globals.node },
 			ecmaVersion: 12,
-			parser: tsparser,
-			parserOptions: {
-				sourceType: 'module',
-			},
+			parser: tsParser,
+			sourceType: 'module',
 		},
+
 		plugins: {
+			'@typescript-eslint': typescriptEslint,
 			'react-hooks': reactHooks,
 			'react-refresh': reactRefresh,
 		},
 		rules: {
-			...reactHooks.configs.recommended.rules,
+			'@typescript-eslint/no-empty-object-type': ['error', { allowObjectTypes: 'always' }],
+			'@typescript-eslint/consistent-type-imports': 'error',
+			'no-duplicate-imports': 'error',
+			'no-unneeded-ternary': 'error',
+			'prefer-object-spread': 'error',
 			'react-refresh/only-export-components': 'warn',
-			'react/jsx-uses-react': 'off',
+			'@typescript-eslint/explicit-function-return-type': 'off',
 			'react/react-in-jsx-scope': 'off',
+			...reactHooks.configs.recommended.rules,
 		},
 		settings: {
 			react: {
